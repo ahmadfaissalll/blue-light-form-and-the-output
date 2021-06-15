6b9aa4bfd74152059229d41a5ed1fe788ed7c751
Ahmad Faisal <81705914+faisal245@users.noreply.github.com>
2021-06-15 19:24:49 +0700

    Add files via upload
    Form blue-light-theme and output 
    
    Note: To see the output open with XAMPP or similar app

---
+++ hasil form.php
@@ -0,0 +1,26 @@
+<html>
+<body>
+
+
+Welcome <?php echo $_GET["nama-depan"]; ?> 
+        <?php echo $_GET["nama-belakang"]; ?>
+<br>
+Email: <?php echo $_GET["email"]; ?>
+<br>
+Umur: <?php echo $_GET["age"]; ?> tahun
+<br>
+Born: <?php echo $_GET["tanggal"]; ?>
+ <?php echo $_GET["bulan"] ?>
+ <?php echo$_GET["tahun"] ?>
+<br>
+Address: <?php echo $_GET["alamat"] ?>
+<br>
+Gender: <?php echo $_GET["kelamin"] ?>
+<br>
+Hobby: <?php echo $_GET["hobi"] ?>
+ <?php echo $_GET["hobi1"] ?>
+ <?php echo $_GET["hobi2"] ?>
+ <?php echo $_GET["hobi3"] ?>
+
+</body>
+</html>
